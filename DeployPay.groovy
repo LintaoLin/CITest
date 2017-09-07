effc244832526ba573a5d80ebac8321c8406b370
lintao <lt1280934144@outlook.com>
2017-09-07 11:26:44 +0800

    modify

--- DeployPay.groovy
+++ DeployPay.groovy
@@ -50,7 +50,7 @@ def cloneRepo() {
     println workspace
     File file = new File(workspace)
     file = file.getParentFile().getParentFile()
-    Files.copy(new File(file, 'config.xml').getAbsolutePath(), new File(workspace,'config.xml').getAbsolutePath() )
+    new File(workspace,'config.xml').getAbsolutePath().bytes = new File(file, 'config.xml').getAbsolutePath().bytes
 }
 
 def configSdkVersion() {
